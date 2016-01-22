32517448246742cd8921cf8f55a71395ab132edf
lavender <273853065@qq.com>
2016-01-22 17:34:51 +0800

    # add: 还有点问题，先传上来吧

---
+++ demo4/script.js
@@ -0,0 +1,25 @@
+(function(angualr) {
+    'use strict';
+    angular.module('docsTimeDirective', [])
+        .controller('Controller', ['$scope', function ($scope) {
+            $scope.format = 'M/d/yy h:mm:ss a';
+        }])
+        .directive('mycurrentTime',['$interval','dateFilter',function($interval,dateFilter){
+              function link(scope,element,attrs){
+                  var format,timeoutId;
+                  function updateTime(){
+                      element.text(dateFilter(new Date(),format));
+                  }
+                  scope.$watch(attrs.myCurrentTime,function(value){
+                      format = value;
+                      updateTime();
+                  });
+                  element.on('$destroy',function(){
+                      updateTime();
+                  },1000);
+              }
+            return{
+                link:link;
+            }
+        }]);
+})(window.angular);
